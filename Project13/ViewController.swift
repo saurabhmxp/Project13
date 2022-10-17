6e77d49e55a21c717369490bbfd96bd1cc883677
Saurabh <saurabh.agarwal@mxplayer.in>
2022-10-17 15:20:09 +0530

    build interface & add outlet and action

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
 
